2812cad7ee6c67db48b5686ad1dcb7fa6986a87d
MarcosH911 <marcosh.craft@gmail.com>
2024-10-18 11:34:16 +0200

    Updated to use css variables for colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { signIn } from "@/auth/auth";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button type="submit">Sign in with google</button>
+    </form>
+  );
 }
