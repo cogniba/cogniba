fe7a52af5117ccda97bf331205ceaa6070fb9dc3
MarcosH911 <marcosh.craft@gmail.com>
2024-10-25 16:21:43 +0200

    Refactored settings page

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -2,15 +2,29 @@ import AppearanceSetting from "./(components)/(settings)/AppearanceSetting";
 import ShowFeedbackSettings from "./(components)/(settings)/ShowFeedbackSettings";
 import SettingsCard from "./(components)/SettingsCard";
 import ChangePasswordSettings from "./(components)/(settings)/ChangePasswordSettings";
-import { SettingsType } from "@/database/schemas/settingsTable";
+import { settingsTable, SettingsType } from "@/database/schemas/settingsTable";
+import { createClient } from "@/lib/supabase/server";
+import { db } from "@/database/db";
+import { eq } from "drizzle-orm";
 
 export default async function SettingsPage() {
-  const response = await fetch("/settings/get-settings", { method: "GET" });
-  if (!response.ok) {
-    <div>Error getting settings</div>;
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    return <div>Error getting user</div>;
   }
 
-  const { settings }: { settings: SettingsType } = await response.json();
+  const userId = data.user.id;
+
+  const settings = await db
+    .select()
+    .from(settingsTable)
+    .where(eq(settingsTable.userId, userId))
+    .then((res) => (res.length === 1 ? res[0] : null));
+  if (!settings) {
+    return <div>Error getting settings</div>;
+  }
 
   return (
     <div className="mx-6 my-10 flex flex-col items-center gap-6">
