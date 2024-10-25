8a562e92c0cac70cacccb641c16b83b8857cd7ac
MarcosH911 <marcosh.craft@gmail.com>
2024-10-25 09:10:00 +0200

    Fixed play page

---
+++ src/app/api/game/get-level/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/database/db";
+import { gamesTable } from "@/database/schemas/gamesTable";
+import { createClient } from "@/lib/supabase/server";
+import { desc, eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+      return NextResponse.json(
+        { error: "Failed to get user" },
+        { status: 400 },
+      );
+    }
+
+    const userId = data.user.id;
+
+    const level = await db
+      .select()
+      .from(gamesTable)
+      .where(eq(gamesTable.userId, userId))
+      .orderBy(desc(gamesTable.createdAt))
+      .limit(1)
+      .then((res) => (res.length === 1 ? res[0].newLevel : 1));
+
+    return NextResponse.json({ level }, { status: 200 });
+  } catch (error) {
+    console.error("Error updating settings:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
--- src/app/app/play/page.tsx
+++ src/app/app/play/page.tsx
@@ -1,26 +1,54 @@
-import getUserLevel from "@/database/queries/games/getUserLevel";
-import getUserSettings from "@/database/queries/settings/getUserSettings";
+import { SettingsType } from "@/database/schemas/settingsTable";
 import GameTutorial from "./(components)/(tutorial)/GameTutorial";
 import GameLogic from "./(components)/GameLogic";
 import { UserType } from "@/database/schemas/profilesTable";
 
 export default async function GamePage() {
-  const level = await getUserLevel();
-  const { showFeedback } = await getUserSettings();
+  const getLevelPromise = fetch("/api/settings/get-level", {
+    method: "GET",
+  });
+  const getSettingsPromise = fetch("/api/settings/get-settings", {
+    method: "GET",
+  });
+  const getUserPromise = fetch("/api/user/get-user", { method: "GET" });
 
-  const response = await fetch("/api/user/get-user");
-  if (!response.ok) {
+  const [levelResponse, settingsResponse, userResponse] = await Promise.all([
+    getLevelPromise,
+    getSettingsPromise,
+    getUserPromise,
+  ]);
+
+  if (!levelResponse.ok) {
+    <div>Error getting level</div>;
+  } else if (!settingsResponse.ok) {
+    <div>Error getting settings</div>;
+  } else if (!userResponse.ok) {
     return <div>An error has ocurred</div>;
   }
 
-  const user: UserType = await response.json();
+  const levelPromise = levelResponse.json();
+  const settingsPromise = settingsResponse.json();
+  const userPromise = userResponse.json();
+
+  const [levelData, settingsData, userData] = await Promise.all([
+    levelPromise,
+    settingsPromise,
+    userPromise,
+  ]);
+
+  const level: number = levelData.level;
+  const settings: SettingsType = settingsData.settings;
+  const user: UserType = userData.user;
 
   return (
     <>
       {!user.hasFinishedTutorial ? (
         <GameTutorial startingLevel={level} />
       ) : (
-        <GameLogic startingLevel={level} showFeedbackEnabled={showFeedback} />
+        <GameLogic
+          startingLevel={level}
+          showFeedbackEnabled={settings.showFeedback}
+        />
       )}
     </>
   );
