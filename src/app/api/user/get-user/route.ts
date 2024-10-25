e01bd7fb91699472c5a3ef9af4032a34a97574dd
MarcosH911 <marcosh.craft@gmail.com>
2024-10-25 11:32:15 +0200

    Made some api endpoints dynamic

--- src/app/api/user/get-user/route.ts
+++ src/app/api/user/get-user/route.ts
@@ -4,6 +4,8 @@ import { createClient } from "@/lib/supabase/server";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const supabase = createClient();
