fe32cec152298ac8762bf0666fc0e68abb333367
MarcosH911 <marcosh.craft@gmail.com>
2024-10-30 10:51:48 +0100

    Created singleton to fix database bug

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,22 @@
-import "dotenv/config";
-
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const connectionString = process.env.DATABASE_URL!;
+function singleton<Value>(name: string, value: () => Value): Value {
+  const globalAny: any = global;
+  globalAny.__singletons = globalAny.__singletons || {};
 
-export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+  if (!globalAny.__singletons[name]) {
+    globalAny.__singletons[name] = value();
+  }
+
+  return globalAny.__singletons[name];
+}
+
+function createDatabaseConnection() {
+  const connection = postgres(process.env.DATABASE_URL!, {
+    prepare: false,
+  });
+  return drizzle(connection);
+}
+
+export const db = singleton("db", createDatabaseConnection);
