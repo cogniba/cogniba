5b8a2edba471fcf65456237ca6e0f60fdfabf581
MarcosH911 <marcosh.craft@gmail.com>
2024-11-02 09:38:30 +0100

    Modified screen to allow for errors

--- src/components/SimpleMessageScreen.tsx
+++ src/components/SimpleMessageScreen.tsx
@@ -1,14 +1,23 @@
+import { cn } from "@/lib/cn";
+
 interface SimpleMessageScreenProps {
   mainMessage: React.ReactNode;
   secondaryMessage?: React.ReactNode;
+  variant?: "normal" | "error";
 }
 
 export default function SimpleMessageScreen({
   mainMessage,
   secondaryMessage,
+  variant = "normal",
 }: SimpleMessageScreenProps) {
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
+    <div
+      className={cn(
+        "flex min-h-screen items-center justify-center p-4",
+        variant === "error" && "bg-destructive text-destructive-foreground",
+      )}
+    >
       <div className="flex h-full max-w-3xl flex-col items-center justify-center gap-6 text-center">
         <div className="text-2xl sm:text-3xl">{mainMessage}</div>
         {secondaryMessage && (
