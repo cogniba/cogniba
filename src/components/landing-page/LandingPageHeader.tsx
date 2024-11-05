de82e1bd7989742de0575d711ff3029f131268d0
MarcosH911 <marcosh.craft@gmail.com>
2024-11-05 08:24:54 +0100

    Update LandingPageHeader component for improved navigation and styling

--- src/components/landing-page/LandingPageHeader.tsx
+++ src/components/landing-page/LandingPageHeader.tsx
@@ -13,28 +13,28 @@ export default function LandingPageHeader() {
     <div className="flex w-full justify-center border-b bg-secondary/50 pb-16 shadow-md">
       <NavigationMenu className="fixed mx-auto flex h-16 w-full max-w-7xl justify-between">
         <NavigationMenuList>
-          <NavigationMenuItem className="mr-8 text-[1.375rem] font-bold">
+          <NavigationMenuItem className="mr-9 text-xl font-bold">
             <Link href="/" className="flex items-center justify-center gap-2.5">
               <Logo className="text-2xl" />
               <span className="text-primary">Cogniba</span>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#features">
+            <Link href="#features" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 Features
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#how-it-works">
+            <Link href="#how-it-works" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 How it works
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#pricing">
+            <Link href="#pricing" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 Pricing
               </NavigationMenuLink>
