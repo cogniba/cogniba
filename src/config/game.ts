77c73ecfc6409e7a7ae50e1a84f2fa32c28dbf6a
MarcosH911 <marcosh.craft@gmail.com>
2024-11-16 10:31:46 +0100

    Move game configuration constants to a new file and remove deprecated constants

---
+++ src/config/game.ts
@@ -0,0 +1,8 @@
+export const BASE_SEQUENCE_LENGTH = 20;
+export const NUM_TARGETS = 6;
+export const VISIBLE_SQUARE_DURATION = 1000;
+export const HIDDEN_SQUARE_DURATION = 2000;
+export const DELAY_BEFORE_START = 1500;
+
+export const DECREASE_LEVEL_THRESHOLD = 0.45;
+export const INCREASE_LEVEL_THRESHOLD = 0.8;
--- src/settings/constants.ts
+++
@@ -1,8 +0,0 @@
-export const gameBaseSequenceLength = 20;
-export const gameNumTargets = 6;
-export const gameVisibleSquareDuration = 1000;
-export const gameHiddenSquareDuration = 2000;
-export const gameDelayBeforeStart = 1500;
-
-export const gameDecreaseLevelThreshold = 0.45;
-export const gameIncreaseLevelThreshold = 0.8;
