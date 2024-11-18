bdde009046c4c1ea067c699183b1d2e68ae662ff
MarcosH911 <marcos.hernanz.anton@gmail.com>
2024-11-18 18:43:04 +0100

    Add user ID metadata to Stripe checkout session

--- src/app/api/stripe/checkoutSession/route.ts
+++ src/app/api/stripe/checkoutSession/route.ts
@@ -1,10 +1,23 @@
 import { CURRENCY, PRO_PLAN_PRICE } from "@/config/stripe";
+import { createClient } from "@/lib/supabase/server";
 import { stripe } from "@/lib/stripe";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+      return NextResponse.json(
+        { error: "Failed to get user" },
+        { status: 400 },
+      );
+    }
+
+    const userId = data.user.id;
+
     const origin: string = request.headers.get("origin") as string;
 
     const checkoutSession: Stripe.Checkout.Session =
@@ -23,7 +36,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
             },
           },
         ],
-        // TODO
+        metadata: { userId },
         success_url: `${origin}/TODO?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${origin}/TODO`,
       });
