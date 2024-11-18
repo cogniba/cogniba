e1adecf972d55036f7078c33432c082d070bfd06
MarcosH911 <marcosh.craft@gmail.com>
2024-11-18 09:02:23 +0100

    Use environment-specific secret key for Stripe initialization

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,12 @@ import "server-only";
 
 import { Stripe } from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const secretKey =
+  process.env.NODE_ENV === "production"
+    ? process.env.STRIPE_SECRET_KEY
+    : process.env.STRIPE_TEST_SECRET_KEY;
+
+export const stripe = new Stripe(secretKey!, {
   apiVersion: "2024-10-28.acacia",
   appInfo: {
     name: "Cogniba",
