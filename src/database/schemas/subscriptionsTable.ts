2a43372adca2abb4b9d92dde19ddbd528e5c44be
MarcosH911 <marcos.hernanz.anton@gmail.com>
2024-11-18 19:23:50 +0100

    Refactor subscriptions table schema to use text type for subscriptionId and set default status to inactive

--- src/database/schemas/subscriptionsTable.ts
+++ src/database/schemas/subscriptionsTable.ts
@@ -1,14 +1,17 @@
-import { date, pgEnum, pgTable, uuid } from "drizzle-orm/pg-core";
+import { date, pgEnum, pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { profilesTable } from "./profilesTable";
 
 export const statusEnum = pgEnum("status", ["active", "inactive"]);
 
 export const subscriptionsTable = pgTable("subscriptions", {
+  id: uuid("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
   userId: uuid("user_id")
     .primaryKey()
     .references(() => profilesTable.userId, { onDelete: "cascade" })
     .notNull(),
-  subscriptionId: uuid("subscription_id"),
-  status: statusEnum("status").notNull(),
+  subscriptionId: text("subscription_id").notNull(),
+  status: statusEnum("status").notNull().default("inactive"),
   lastPaymentDate: date("last_payment_date"),
 });
