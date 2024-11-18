1b4265f20ab623f156f8aa1d4b74ea89d037069b
MarcosH911 <marcosh.craft@gmail.com>
2024-11-18 09:02:19 +0100

    Refactor getStripe function to use environment-specific publishable key

--- src/lib/getStripe.ts
+++ src/lib/getStripe.ts
@@ -2,9 +2,14 @@ import { Stripe, loadStripe } from "@stripe/stripe-js";
 
 let stripePromise: Promise<Stripe | null>;
 
+const publishableKey =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+    : process.env.NEXT_PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY;
+
 export default function getStripe() {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+    stripePromise = loadStripe(publishableKey!);
   }
   return stripePromise;
 }
