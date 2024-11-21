075579c2a8c44f64e48400c6984237a08ccd7756
MarcosH911 <marcosh.craft@gmail.com>
2024-11-21 20:52:19 +0100

    Refactor Game component layout for improved readability and structure

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -63,56 +63,54 @@ export default function Game({
   }, [hasIncreasedLevel]);
 
   return (
-    <>
-      <div
-        className={cn(
-          "flex h-screen items-center justify-center transition duration-300 lg:can-hover:-ml-16",
-          feedback === "correct" && "bg-green-200/80 dark:bg-green-950",
-          feedback === "incorrect" && "bg-red-200/80 dark:bg-red-950",
-          feedback === "missed" && "bg-yellow-200/50 dark:bg-yellow-950/70",
-        )}
-      >
-        {!hideConfetti && (
-          <Confetti
-            style={{ zIndex: 100 }}
-            height={height}
-            width={width}
-            numberOfPieces={300}
-            tweenDuration={10000}
-            recycle={showConfetti}
+    <div
+      className={cn(
+        "flex h-screen items-center justify-center transition duration-300 lg:can-hover:-ml-16",
+        feedback === "correct" && "bg-green-200/80 dark:bg-green-950",
+        feedback === "incorrect" && "bg-red-200/80 dark:bg-red-950",
+        feedback === "missed" && "bg-yellow-200/50 dark:bg-yellow-950/70",
+      )}
+    >
+      {!hideConfetti && (
+        <Confetti
+          style={{ zIndex: 100 }}
+          height={height}
+          width={width}
+          numberOfPieces={300}
+          tweenDuration={10000}
+          recycle={showConfetti}
+        />
+      )}
+      {hasReachedNewLevel && level < 5 ? (
+        <GameNewLevelScreen
+          level={level}
+          setIsVisible={setHasReachedNewLevel}
+        />
+      ) : (
+        <StartScreen
+          visible={isStartScreenVisible}
+          onStart={startPlaying}
+          correctHits={correctHits}
+          incorrectHits={incorrectHits}
+          missedHits={missedHits}
+          previousLevel={previousLevel}
+          newLevel={level}
+        />
+      )}
+      <div className="flex h-full w-full max-w-3xl flex-col items-center justify-center px-[4cqw] [container-type:size] md:px-2">
+        <div className="my-[1.5cqh] flex-shrink-0 sm:mb-[2.5cqh] sm:mt-[1.5cqh]">
+          <GameLevelDisplay level={level} />
+        </div>
+        <div className="h-[100cqmin] w-[100cqmin]">
+          <GameBoard selectedSquare={selectedSquare} />
+        </div>
+        <div className="mb-[2.5cqh] mt-[3cqh] h-[11cqh] w-[100cqmin] max-w-5xl flex-shrink-0 2xl:w-[max(100cqmin,90vh)]">
+          <GameButton
+            isButtonPressed={isButtonPressed}
+            handleButtonPress={handleButtonPress}
           />
-        )}
-        {hasReachedNewLevel && level < 5 ? (
-          <GameNewLevelScreen
-            level={level}
-            setIsVisible={setHasReachedNewLevel}
-          />
-        ) : (
-          <StartScreen
-            visible={isStartScreenVisible}
-            onStart={startPlaying}
-            correctHits={correctHits}
-            incorrectHits={incorrectHits}
-            missedHits={missedHits}
-            previousLevel={previousLevel}
-            newLevel={level}
-          />
-        )}
-        <div className="flex h-full w-full max-w-3xl flex-col items-center justify-center px-[4cqw] [container-type:size] md:px-2">
-          <div className="my-[1.5cqh] flex-shrink-0 sm:mb-[2.5cqh] sm:mt-[1.5cqh]">
-            <GameLevelDisplay level={level} />
-          </div>
-          <div className="h-[100cqmin] w-[100cqmin]">
-            <GameBoard selectedSquare={selectedSquare} />
-          </div>
-          <div className="mb-[2.5cqh] mt-[3cqh] h-[11cqh] w-[100cqmin] max-w-5xl flex-shrink-0 2xl:w-[max(100cqmin,90vh)]">
-            <GameButton
-              isButtonPressed={isButtonPressed}
-              handleButtonPress={handleButtonPress}
-            />
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
