9aa39fb891c4004cca5db78910846aaf30781aba
MarcosH911 <marcosh.craft@gmail.com>
2024-11-21 20:37:45 +0100

    Fix loading indicator height in AnalyticsLoading component

--- src/app/app/analytics/loading.tsx
+++ src/app/app/analytics/loading.tsx
@@ -11,11 +11,11 @@ export default function AnalyticsLoading() {
               <div className="flex w-full flex-col items-center justify-start gap-3 sm:w-fit lg:flex-row">
                 <div className="h-10 w-full animate-pulse rounded-md border border-input bg-secondary/30 px-3 py-2 sm:w-52 md:w-64"></div>
               </div>
-              <div className="h-full w-full animate-pulse rounded-md border border-input bg-background bg-secondary/30 sm:w-64"></div>
+              <div className="h-10 w-full animate-pulse rounded-md border border-input bg-background bg-secondary/30 sm:w-64"></div>
             </div>
           </CardHeader>
           <CardContent className="flex h-full flex-col p-5 sm:p-8">
-            <div className="flex h-10 w-full items-center justify-center pb-20">
+            <div className="flex h-full w-full items-center justify-center pb-20">
               <Loader2Icon className="absolute z-50 size-10 animate-spin" />
             </div>
           </CardContent>
