59f840e03e6abd3a54b08f5d4461d598b50e30d8
MarcosH911 <marcosh.craft@gmail.com>
2024-11-21 08:49:51 +0100

    Update createUrl function to handle production and development environments

--- src/lib/createUrl.ts
+++ src/lib/createUrl.ts
@@ -1,11 +1,15 @@
 export default function createUrl(pathname: string) {
-  const isAppSubdomain = pathname.startsWith("/app");
-  const formattedPathname = isAppSubdomain
-    ? pathname.replace(/^\/app/, "") || "/"
-    : pathname;
-  const baseUrl = isAppSubdomain
-    ? process.env.NEXT_PUBLIC_APP_URL!
-    : process.env.NEXT_PUBLIC_SITE_URL!;
+  if (process.env.NODE_ENV === "production") {
+    const isAppSubdomain = pathname.startsWith("/app");
+    const formattedPathname = isAppSubdomain
+      ? pathname.replace(/^\/app/, "") || "/"
+      : pathname;
+    const baseUrl = isAppSubdomain
+      ? process.env.NEXT_PUBLIC_APP_URL!
+      : process.env.NEXT_PUBLIC_SITE_URL!;
 
-  return new URL(formattedPathname, baseUrl);
+    return new URL(formattedPathname, baseUrl);
+  } else {
+    return new URL(pathname, process.env.NEXT_PUBLIC_SITE_URL!);
+  }
 }
