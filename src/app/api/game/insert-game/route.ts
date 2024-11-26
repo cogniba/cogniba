1d1db3909bf9006bc55efaf4255a300fb9d4d3df
MarcosH911 <marcosh.craft@gmail.com>
2024-11-26 10:53:53 +0100

    Implement conditional revalidation paths based on environment in insert-game route

--- src/app/api/game/insert-game/route.ts
+++ src/app/api/game/insert-game/route.ts
@@ -71,13 +71,13 @@ export async function POST(request: Request) {
     });
 
     console.log("AAA");
-    // if (process.env.NODE_ENV === "production") {
-    // revalidatePath("/play");
-    // revalidatePath("/analytics");
-    // } else {
-    revalidatePath("/app/play");
-    revalidatePath("/app/analytics");
-    // }
+    if (process.env.NODE_ENV === "production") {
+      revalidatePath("/play");
+      revalidatePath("/analytics");
+    } else {
+      revalidatePath("/app/play");
+      revalidatePath("/app/analytics");
+    }
 
     return NextResponse.json(
       { message: "Game inserted successfully" },
