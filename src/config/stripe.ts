95901b1d20cb1761d3bbdecead23edf7714ebdbc
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-01-30 12:05:15 +0100

    Add Stripe configuration with plan types and details

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,2 +1,37 @@
-export const CURRENCY = "usd";
-export const PRO_PLAN_PRICE = 20;
+export type PlanType = {
+  priceId: string;
+  price: number;
+  isFreePlan?: boolean;
+
+  name: string;
+  description: string;
+  features: string[];
+
+  highlighted?: boolean;
+};
+
+const stripeConfig = {
+  plans: [
+    {
+      priceId: "",
+      price: 0,
+      isFreePlan: true,
+
+      name: "Free",
+      description: "Great for beginners exploring Cogniba.",
+      features: ["10 games per day", "30-day analytics history"],
+    },
+    {
+      priceId: "<PRICE_ID>",
+      price: 20,
+
+      name: "Pro",
+      description: "For serious users aiming for top results.",
+      features: ["Unlimited games", "Unlimited analytics history"],
+
+      highlighted: true,
+    },
+  ] satisfies PlanType[],
+};
+
+export default stripeConfig;
