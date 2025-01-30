1cfb7c7e19b165dd169a25f4ac3d15641bb15ae4
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-01-30 12:02:50 +0100

    Remove obsolete migration files for subscriptions and related metadata

--- supabase/migrations/0011_living_richard_fisk.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "subscriptions" ALTER COLUMN "subscription_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "subscription_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "status" SET DEFAULT 'inactive';--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD COLUMN "id" uuid;
