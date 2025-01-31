9c3060f11b25ae2fdd8e4951cbfd17034e85155f
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-01-31 18:41:28 +0100

    Add SVG support for LogoIcon component and update webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
+      test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
 };
--- src/components/svg/LogoIcon.tsx
+++ src/components/svg/LogoIcon.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Logo from "@/public/logo.svg";
-import Image from "next/image";
-
 interface LogoIconProps {
   className?: string;
 }
@@ -10,7 +7,32 @@ interface LogoIconProps {
 export default function LogoIcon({ className }: LogoIconProps) {
   return (
     <div className={className}>
-      <Image src={Logo} alt="Logo" />
+      <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 1080 1080"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_1_2)">
+          <rect
+            x="9"
+            y="9"
+            width="1062"
+            height="1062"
+            rx="141"
+            fill="#F97316"
+            stroke="#EA580C"
+            strokeWidth="18"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_2">
+            <rect width="1080" height="1080" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
     </div>
   );
 }
