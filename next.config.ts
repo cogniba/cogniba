512a70a9f8e51a5ec5135fcaaee9f429cdde244e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-01 10:25:14 +0100

    Enhance SVG handling in webpack configuration to support JSX and TSX file issuers

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
+
     return config;
   },
 };
