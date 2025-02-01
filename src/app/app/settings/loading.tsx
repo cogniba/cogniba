d4a2151129d55ffc6396557ea29b92ab61257897
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-01 16:52:38 +0100

    Add billing section to SettingsLoading component

--- src/app/app/settings/loading.tsx
+++ src/app/app/settings/loading.tsx
@@ -14,6 +14,7 @@ export default function SettingsLoading() {
           </div>
         </div>
       </div>
+
       <div className="flex w-full max-w-md flex-col divide-y rounded-lg border p-8 shadow-sm *:py-8 first:*:pt-0 last:*:pb-0 md:max-w-6xl">
         <div className="flex flex-col items-start justify-between gap-6 md:flex-row md:items-center md:gap-8">
           <div className="flex flex-col gap-0.5">
@@ -30,6 +31,7 @@ export default function SettingsLoading() {
           </div>
         </div>
       </div>
+
       <div className="flex w-full max-w-md flex-col divide-y rounded-lg border p-8 shadow-sm *:py-8 first:*:pt-0 last:*:pb-0 md:max-w-6xl">
         <div className="flex flex-col items-start justify-between gap-6 md:flex-row md:items-center md:gap-8">
           <div className="flex flex-col gap-0.5">
@@ -38,6 +40,20 @@ export default function SettingsLoading() {
             </span>
             <span className="text-sm text-muted-foreground">
               Change your password
+            </span>
+          </div>
+          <div className="w-full flex-shrink-0 md:w-60 lg:w-80">
+            <div className="h-10 w-full animate-pulse rounded-md border border-input bg-secondary/30"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex w-full max-w-md flex-col divide-y rounded-lg border p-8 shadow-sm *:py-8 first:*:pt-0 last:*:pb-0 md:max-w-6xl">
+        <div className="flex flex-col items-start justify-between gap-6 md:flex-row md:items-center md:gap-8">
+          <div className="flex flex-col gap-0.5">
+            <span className="text-lg font-semibold md:text-xl">Billing</span>
+            <span className="text-sm text-muted-foreground">
+              Manage your billing information
             </span>
           </div>
           <div className="w-full flex-shrink-0 md:w-60 lg:w-80">
