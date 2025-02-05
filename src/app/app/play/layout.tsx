a92a73928198e444a305e20de22e9644c3d50c07
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-05 20:03:47 +0100

    Fetch game data asynchronously in GameLayout and handle errors before rendering context providers

--- src/app/app/play/layout.tsx
+++ src/app/app/play/layout.tsx
@@ -1,13 +1,33 @@
+import redirectToError from "@/actions/redirectToError";
 import GameContextProvider from "@/context/GameContext";
 import GameTutorialContextProvider from "@/context/GameTutorialContext";
+import fetchGameData from "@/lib/game/fetchGameData";
 
 interface GameLayoutProps {
   children: React.ReactNode;
 }
 
-export default function GameLayout({ children }: GameLayoutProps) {
+export default async function GameLayout({ children }: GameLayoutProps) {
+  const { level, maxLevel, showFeedback, hasFinishedTutorial, error } =
+    await fetchGameData();
+  if (
+    error ||
+    !level ||
+    !maxLevel ||
+    showFeedback === undefined ||
+    hasFinishedTutorial === undefined
+  ) {
+    redirectToError("Error getting game data");
+    return;
+  }
+
   return (
-    <GameContextProvider>
+    <GameContextProvider
+      startingLevel={level}
+      maxLevel={maxLevel}
+      hasFinishedTutorial={hasFinishedTutorial}
+      showFeedbackEnabled={showFeedback}
+    >
       <GameTutorialContextProvider>{children}</GameTutorialContextProvider>
     </GameContextProvider>
   );
