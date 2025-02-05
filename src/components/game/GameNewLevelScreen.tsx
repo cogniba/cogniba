2ad15c3b59ab6474769252bde76e7874ff35fb4c
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-05 10:47:28 +0100

    Refactor GameNewLevelScreen to utilize steps from GameTutorialContext and remove redundant getSteps function

--- src/components/game/GameNewLevelScreen.tsx
+++ src/components/game/GameNewLevelScreen.tsx
@@ -2,59 +2,13 @@
 
 import { useEffect } from "react";
 import GameTutorialSteps from "./tutorial/GameTutorialSteps";
-import { Strong } from "@/components/ui/Strong";
 import sleep from "@/lib/sleep";
 import { useGameContext } from "@/context/GameContext";
-import { StepType } from "@/config/gameTutorial";
 import { useGameTutorialContext } from "@/context/GameTutorialContext";
 
-function getSteps(level: number) {
-  return [
-    {
-      title: (
-        <>
-          You reached <Strong variant="primary"> level {level}</Strong>!
-        </>
-      ),
-      content: (
-        <>
-          <Strong>Congratulations</Strong> on reaching{" "}
-          <Strong variant="primary">level {level}</Strong>, you are{" "}
-          <Strong>doing great!</Strong>
-        </>
-      ),
-      placement: "center",
-      target: "body",
-      hideBackButton: true,
-    },
-    {
-      title: (
-        <>
-          How to play <Strong variant="primary">level {level}</Strong>
-        </>
-      ),
-      content: (
-        <>
-          You are now on <Strong variant="primary">level {level}</Strong>, so
-          you will have to <Strong>press the button</Strong> when the square
-          appears <Strong>in the same spot as</Strong>{" "}
-          <Strong variant="primary">{level} steps</Strong>{" "}
-          <Strong>before</Strong>.
-        </>
-      ),
-      placement: "center",
-      target: "body",
-      hideBackButton: true,
-      primaryButtonText: "Okay",
-    },
-  ] satisfies StepType[];
-}
-
 export default function GameNewLevelScreen() {
-  const { level, setHasReachedNewLevel } = useGameContext();
-  const { step } = useGameTutorialContext();
-
-  const steps = getSteps(level);
+  const { setHasReachedNewLevel } = useGameContext();
+  const { step, steps } = useGameTutorialContext();
 
   useEffect(() => {
     const handleHideScreen = async () => {
