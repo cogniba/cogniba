f0891f6978db224c2c5c72ad0eda7e8fcb71d6fe
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-06 08:44:59 +0100

    Reset tutorial step to 0 when hiding the new level screen

--- src/components/game/GameNewLevelScreen.tsx
+++ src/components/game/GameNewLevelScreen.tsx
@@ -2,24 +2,25 @@
 
 import { useEffect } from "react";
 import GameTutorialSteps from "./tutorial/GameTutorialSteps";
-import sleep from "@/lib/sleep";
 import { useGameContext } from "@/context/GameContext";
 import { useGameTutorialContext } from "@/context/GameTutorialContext";
+import sleep from "@/lib/sleep";
 
 export default function GameNewLevelScreen() {
   const { setHasReachedNewLevel } = useGameContext();
-  const { step, steps } = useGameTutorialContext();
+  const { step, setStep, steps } = useGameTutorialContext();
 
   useEffect(() => {
     const handleHideScreen = async () => {
       await sleep(500);
       setHasReachedNewLevel(false);
+      setStep(0);
     };
 
     if (step === steps.length) {
       handleHideScreen();
     }
-  }, [step, setHasReachedNewLevel, steps.length]);
+  }, [step, setHasReachedNewLevel, steps.length, setStep]);
 
   return <GameTutorialSteps showSkipButton={false} />;
 }
