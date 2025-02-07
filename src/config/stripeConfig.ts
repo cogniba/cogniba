0efa790ed95fd21071083f846818a04a3a6b7d49
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-07 08:36:12 +0100

    Enhance stripeConfig with DeepReadonly type for improved type safety

--- src/config/stripeConfig.ts
+++ src/config/stripeConfig.ts
@@ -1,14 +1,18 @@
+import { DeepReadonly } from "@/lib/typescript/DeepReadOnly";
+
 export type PlanType = {
   priceId: string;
   price: number;
   isFreePlan?: boolean;
-
   name: string;
   description: string;
   features: string[];
-
   highlighted?: boolean;
 };
+
+interface StripeConfig {
+  plans: PlanType[];
+}
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
@@ -18,24 +22,21 @@ const stripeConfig = {
       priceId: "",
       price: 0,
       isFreePlan: true,
-
       name: "Free",
       description: "Great for beginners exploring Cogniba.",
-      features: ["Feature 1", "Feature 2", "Feature 3"],
+      features: ["Feature 1", "Feature 2", "Feature 3"] as const,
     },
     {
       priceId: isDevelopment
         ? "price_1Qnd87RprLy2N53jYmRQE4DC"
         : "price_1QndAKRprLy2N53jEjWArTKJ",
       price: 20,
-
       name: "Pro",
       description: "For serious users aiming for top results.",
-      features: ["Feature 1", "Feature 2", "Feature 3"],
-
+      features: ["Feature 1", "Feature 2", "Feature 3"] as const,
       highlighted: true,
     },
   ] satisfies PlanType[],
-};
+} as DeepReadonly<StripeConfig>;
 
 export default stripeConfig;
