8ecee04b1b3c0365403d0519c651d5b2f13100b8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-09 11:22:40 +0100

    Refactor Analytics component to remove state management and simplify rendering

--- src/components/analytics/Analytics.tsx
+++ src/components/analytics/Analytics.tsx
@@ -1,51 +1,18 @@
 "use client";
 
 import AnalyticsFilters from "./AnalyticsFilters";
-import cleanChartData from "@/lib/cleanChartData";
 import AnalyticsMetrics from "./AnalyticsMetricCharts";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useState } from "react";
-import { subDays } from "date-fns";
 
-import type { DateRange } from "react-day-picker";
-import { GamesData } from "@/app/api/analytics/get-data/route";
-
-export type ChartMetrics =
-  | "level"
-  | "accuracy"
-  | "stats"
-  | "gamesPlayed"
-  | "timePlayed";
-
-interface AnalyticsProps {
-  data: GamesData;
-}
-
-export default function Analytics({ data }: AnalyticsProps) {
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: subDays(new Date(), 7),
-    to: new Date(),
-  });
-  const [chartMetric, setChartMetric] = useState<ChartMetrics>("level");
-
-  const cleanData =
-    date && date.from && date.to && data && data.length > 0
-      ? cleanChartData(data, date.from, date.to)
-      : null;
-
+export default function Analytics() {
   return (
     <Card className="flex h-full w-full flex-col pb-4 pt-8 xs:py-0">
       <CardHeader className="border-b p-5 sm:p-8">
-        <AnalyticsFilters
-          date={date}
-          setDate={setDate}
-          chartMetric={chartMetric}
-          setChartMetric={setChartMetric}
-        />
+        <AnalyticsFilters />
       </CardHeader>
       <CardContent className="flex h-full flex-col p-5 sm:p-8">
-        <AnalyticsMetrics chartMetric={chartMetric} data={cleanData} />
+        <AnalyticsMetrics />
       </CardContent>
     </Card>
   );
