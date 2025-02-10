b9c7c7c04100c2a02d413df5f08cf68084851603
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-10 19:15:03 +0100

    Refactor getLevel function for improved error handling and code clarity

--- src/actions/game/getLevel.ts
+++ src/actions/game/getLevel.ts
@@ -9,26 +9,22 @@ export default async function getLevel(): Promise<{
   level?: number;
   error?: string;
 }> {
-  try {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) {
-      return { error: "Failed to get user" };
-    }
-
-    const level = await db
-      .select()
-      .from(gamesTable)
-      .where(eq(gamesTable.userId, user.id))
-      .orderBy(desc(gamesTable.createdAt))
-      .limit(1)
-      .then((res) => (res.length === 1 ? res[0].newLevel : 1));
-
-    return { level };
-  } catch {
-    return { error: "An unexpected error occurred" };
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return { error: "Failed to get user" };
   }
+
+  const level = await db
+    .select()
+    .from(gamesTable)
+    .where(eq(gamesTable.userId, user.id))
+    .orderBy(desc(gamesTable.createdAt))
+    .limit(1)
+    .then((res) => (res.length === 1 ? res[0].newLevel : 1));
+
+  return { level };
 }
