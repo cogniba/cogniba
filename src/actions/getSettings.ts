cce192bacab2a686a8d8f2cc5c006726567421dc
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-10 19:24:16 +0100

    Refactor getSettings function for improved error handling and code clarity

--- src/actions/getSettings.ts
+++ src/actions/getSettings.ts
@@ -9,23 +9,28 @@ export default async function getSettings(): Promise<{
   settings?: SettingsType;
   error?: string;
 }> {
-  const supabase = await createClient();
+  try {
+    const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) {
-    return { error: "Failed to get user" };
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      return { error: "Failed to get user" };
+    }
+
+    const settings = await db
+      .select()
+      .from(settingsTable)
+      .where(eq(settingsTable.userId, user.id))
+      .then((res) => (res.length === 1 ? res[0] : null));
+    if (settings === null) {
+      return { error: "Failed to get settings" };
+    }
+
+    return { settings };
+  } catch (error) {
+    console.error(error);
+    return { error: "An unexpected error occurred" };
   }
-
-  const settings = await db
-    .select()
-    .from(settingsTable)
-    .where(eq(settingsTable.userId, user.id))
-    .then((res) => (res.length === 1 ? res[0] : null));
-  if (settings === null) {
-    return { error: "Failed to get settings" };
-  }
-
-  return { settings };
 }
