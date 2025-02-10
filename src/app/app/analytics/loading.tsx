4d71d10e170917fea9e482db5d74ef996e7fb1b3
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-10 11:07:36 +0100

    Refactor AnalyticsLoading component to use full height for improved layout consistency

--- src/app/app/analytics/loading.tsx
+++ src/app/app/analytics/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2Icon } from "lucide-react";
 
 export default function AnalyticsLoading() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-full items-center justify-center">
       <div className="flex h-full w-full max-w-7xl flex-col items-center gap-5 xs:mx-6 xs:py-10 sm:mx-10">
         <Card className="flex h-full w-full flex-col pb-4 pt-8 xs:py-0">
           <CardHeader className="border-b p-5 sm:p-8">
@@ -11,7 +11,7 @@ export default function AnalyticsLoading() {
               <div className="flex w-full flex-col items-center justify-start gap-3 sm:w-fit lg:flex-row">
                 <div className="h-10 w-full animate-pulse rounded-md border border-input bg-secondary/30 px-3 py-2 sm:w-52 md:w-64"></div>
               </div>
-              <div className="h-10 w-full animate-pulse rounded-md border border-input bg-background bg-secondary/30 sm:w-64"></div>
+              <div className="h-10 w-full animate-pulse rounded-md border border-input bg-secondary/30 sm:w-64"></div>
             </div>
           </CardHeader>
           <CardContent className="flex h-full flex-col p-5 sm:p-8">
