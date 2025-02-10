fb99a6c4c5d677735093b58c1a1c7d260c793658
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-10 19:15:06 +0100

    Refactor getMaxLevel function for improved error handling and code clarity

--- src/actions/game/getMaxLevel.ts
+++ src/actions/game/getMaxLevel.ts
@@ -9,26 +9,22 @@ export default async function getMaxLevel(): Promise<{
   maxLevel?: number;
   error?: string;
 }> {
-  try {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) {
-      return { error: "Failed to get user" };
-    }
-
-    const maxLevel = await db
-      .select()
-      .from(gamesTable)
-      .where(eq(gamesTable.userId, user.id))
-      .orderBy(desc(gamesTable.newLevel))
-      .limit(1)
-      .then((res) => (res.length === 1 ? res[0].newLevel : 1));
-
-    return { maxLevel };
-  } catch {
-    return { error: "An unexpected error occurred" };
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return { error: "Failed to get user" };
   }
+
+  const maxLevel = await db
+    .select()
+    .from(gamesTable)
+    .where(eq(gamesTable.userId, user.id))
+    .orderBy(desc(gamesTable.newLevel))
+    .limit(1)
+    .then((res) => (res.length === 1 ? res[0].newLevel : 1));
+
+  return { maxLevel };
 }
