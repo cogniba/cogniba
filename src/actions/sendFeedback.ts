3db91acd9fb73b7d4fe2ce17216024d4fe90f465
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-10 19:24:19 +0100

    Refactor sendFeedback function for improved error handling and code clarity

--- src/actions/sendFeedback.ts
+++ src/actions/sendFeedback.ts
@@ -11,32 +11,37 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default async function sendFeedback(
   formData: FeedbackSchemaType,
 ): Promise<{ error?: string }> {
-  const supabase = await createClient();
+  try {
+    const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) {
-    const error = new Error("User not found");
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      const error = new Error("User not found");
+      console.error(error);
+      return { error: error.message };
+    }
+
+    const { type, message } = formData;
+    const userId = user.id;
+
+    await db.insert(feedbackTable).values({
+      type,
+      message,
+      userId,
+    });
+
+    await resend.emails.send({
+      from: process.env.FEEDBACK_EMAIL!,
+      to: process.env.FEEDBACK_EMAIL!,
+      subject: `New Feedback: ${type}`,
+      text: `New feedback received.\n\n\nType:\n${type}\n\nMessage:\n${message}\n\nUser ID:\n${userId}`,
+    });
+
+    return {};
+  } catch (error) {
     console.error(error);
-    return { error: error.message };
+    return { error: "An unexpected error occurred" };
   }
-
-  const { type, message } = formData;
-  const userId = user.id;
-
-  await db.insert(feedbackTable).values({
-    type,
-    message,
-    userId,
-  });
-
-  await resend.emails.send({
-    from: process.env.FEEDBACK_EMAIL!,
-    to: process.env.FEEDBACK_EMAIL!,
-    subject: `New Feedback: ${type}`,
-    text: `New feedback received.\n\n\nType:\n${type}\n\nMessage:\n${message}\n\nUser ID:\n${userId}`,
-  });
-
-  return {};
 }
