774056524f65d98cde51df2887ccc2c7ebfbe75f
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-12 16:58:14 +0100

    Integrate SuspendedPostHogPageView component for improved page view tracking

--- src/components/providers/PostHogProvider.tsx
+++ src/components/providers/PostHogProvider.tsx
@@ -2,6 +2,7 @@
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
+import SuspendedPostHogPageView from "../PostHogPageView";
 
 interface PostHogProviderProps {
   children: React.ReactNode;
@@ -12,8 +13,14 @@ export default function PostHogProvider({ children }: PostHogProviderProps) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
       person_profiles: "always",
+      capture_pageview: false,
     });
   }, []);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>;
+  return (
+    <PHProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PHProvider>
+  );
 }
