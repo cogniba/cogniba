4198171d86f00ff8ae2b3768f6a2aa55cc5ff8a2
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-02-12 16:52:58 +0100

    Add PostHogProvider component for analytics integration

---
+++ src/components/providers/PostHogProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+
+interface PostHogProviderProps {
+  children: React.ReactNode;
+}
+
+export function PostHogProvider({ children }: PostHogProviderProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      person_profiles: "always",
+    });
+  }, []);
+
+  return <PHProvider client={posthog}>{children}</PHProvider>;
+}
