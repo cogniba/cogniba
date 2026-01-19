067a2f2e5fae03574f988b998a06b3350eb9037f
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-01-19 20:40:21 +0100

    feat: add MDX options for remark and rehype plugins in mdxToHtml function

--- src/lib/markdown/mdxToHtml.ts
+++ src/lib/markdown/mdxToHtml.ts
@@ -1,10 +1,18 @@
 import { compileMDX } from "next-mdx-remote/rsc";
 import MDXComponents from "@/components/blog/mdx/MDXComponents";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 export default async function mdxToHtml(source: string) {
   const { content } = await compileMDX({
     source,
-    options: { parseFrontmatter: true },
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeSlug],
+      },
+    },
     components: MDXComponents,
   });
 
